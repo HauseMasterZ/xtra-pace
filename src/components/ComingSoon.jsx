1c21da4896d9606865bd185649f111420e6c6ce0
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2024-08-08 06:32:49 +0530

    Update layout and logo

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -4,7 +4,6 @@ import { collection, addDoc, doc, getDoc } from "firebase/firestore";
 import { db } from '../../firebaseConfig';
 import CountDownTimer from './CountDownTimer';
 import '../static/styles/ComingSoon.scss';
-
 const ComingSoon = () => {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
@@ -58,39 +57,49 @@ const ComingSoon = () => {
     };
 
     return (
-        <div className="coming-soon">
-            <video autoPlay loop muted className="background-video">
-                <source src="/background_video.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-            <div className="logo-container">
-                <div className="logo-text">
-                    <img src="/bw.png" alt="Website Logo" className="logo" />
-                    <span className="tagline">Game on Get Fit!</span>
+        <div className="container">
+            <div className="left">
+                <div className="coming-soon">
+                    <video autoPlay loop muted className="background-video">
+                        <source src="/background_video.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <div className="logo-container">
+                        <div className="logo-text">
+                            {/* <img src="/bw.png" alt="Website Logo" className="logo" /> */}
+                            <span className='hero-text'>XtraPace</span>
+                            <span className="tagline">Game on Get Fit!</span>
+                        </div>
+                    </div>
+                    <div className="timer-container">
+                        <h1>Website Coming Soon!</h1>
+                    </div>
+                    <p>Get notified when we launch</p>
+                    {/* <CountDownTimer
+                        days={timerTime.days}
+                        hours={timerTime.hours}
+                        minutes={timerTime.minutes}
+                        seconds={timerTime.seconds}
+                    /> */}
+                    <div className="email-content">
+                        <div className="input-box">
+                            <input
+                                type="email"
+                                placeholder="Enter your email..."
+                                value={email}
+                                onChange={handleEmailChange}
+                            />
+                            <button onClick={handleEmailSubmit}>Notify Me</button>
+                        </div>
+                        <p>*Don't worry we will not spam you</p>
+                        {message && <p>{message}</p>}
+                    </div>
                 </div>
-            </div>
-            <div className="timer-container">
-                <h1>Website Coming Soon!</h1>
-            </div>
-            <p>We are working hard to give you a better experience. Stay tuned!</p>
-            <CountDownTimer
-                days={timerTime.days}
-                hours={timerTime.hours}
-                minutes={timerTime.minutes}
-                seconds={timerTime.seconds}
-            />
-            <div className="email-content">
-                <p>Subscribe now to get the latest updates!</p>
-                <div className="input-box">
-                    <input
-                        type="email"
-                        placeholder="Enter your email..."
-                        value={email}
-                        onChange={handleEmailChange}
-                    />
-                    <button onClick={handleEmailSubmit}>Notify Me</button>
+                <div className="right">
+                    <div className="image-container">
+                        <img src={"/prototype.webp"} alt="Product" className='product' />
+                    </div>
                 </div>
-                {message && <p>{message}</p>}
             </div>
         </div>
     );
